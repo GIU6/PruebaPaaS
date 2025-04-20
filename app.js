5a95073c1a0bc05569da9fd924a8f31eeafbefc2
GIU6 <giulianabernal25@gmail.com>
2025-04-19 21:08:28 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express = require('express');
+const {CosmoClieb¿nt}
 const app = express();
 const port=process.env.PORT || 3000;
+
+
+//CONFIGURACIÓN DE COSMO BD
+const cosmos.Client-new CosmoClient(process.env.COSMO_BD_CONNETION_STRNG);
+const database-cosmoClien.databaset(''
 app.get('/', (req, res) => {
     res.send('Hola estamos en SENATI desplegando nuestro PaaS 2025')
 });
-
+//CONFIGURACIÓN DE COSMO BD
 app.listen(port, () => {
     console.log('Aplicacion esuchando en el puerto: ',port)
 });
