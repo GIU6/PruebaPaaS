cafa157c19704ac4856eb6ccbb0f5f2f93924961
GIU6 <giulianabernal25@gmail.com>
2025-04-19 21:15:15 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,36 @@
 const express = require('express');
-const {CosmoClieb¿nt}
-const app = express();
-const port=process.env.PORT || 3000;
+const (CosmosClient)=require('@azure/cosmos');
+const app = express(); 
+const port = process.env.PORT || 3000;
+
+//Configuracion de cosmos db
+const cosmosClient= new CosmosClient(process.env.COSMOS_DB_CONNECTION_STRING);
+const database=cosmoClient.database('academic-db');
+const container=database.container('student');
+
+app.use(express.json());
+//ruta para insertar datos
+app.post('/student',async(req,res)=>(
+   try{
+        const{body}=req;
+        const{resource:createdItem}=await container.items.create(body);
+        res.status(201).send(createdItem);
+    catch(error){
+        res.status(500).send(error);
+    }
+    
+));
+
+app.get('/students', (req, res) =>(
+    try{
+        const{resources}=await cotainer.items.readAll().fetchAll();
+        res.status(200).send(resources);
+    }catch(error){
+        res.status(500).send(error);
+    }
+));
 
 
-//CONFIGURACIÓN DE COSMO BD
-const cosmos.Client-new CosmoClient(process.env.COSMO_BD_CONNETION_STRNG);
-const database-cosmoClien.databaset(''
-app.get('/', (req, res) => {
-    res.send('Hola estamos en SENATI desplegando nuestro PaaS 2025')
-});
-//CONFIGURACIÓN DE COSMO BD
-app.listen(port, () => {
-    console.log('Aplicacion esuchando en el puerto: ',port)
+app.listen(port, ()=>{
+    console.log('Aplicacion escuchando en el puerto: ',port)
 });
